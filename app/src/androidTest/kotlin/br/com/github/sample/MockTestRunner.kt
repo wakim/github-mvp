f0e03b7c680fb49c7fd9c7fe1272847e6f9b4b5f
Wakim <wakim.jraige@gmail.com>
2016-09-04 18:51:51 -0300

    Remove unnecessary code. Move keyguard unlock code to before instrumentations tests run.

--- app/src/androidTest/kotlin/br/com/github/sample/MockTestRunner.kt
+++ app/src/androidTest/kotlin/br/com/github/sample/MockTestRunner.kt
@@ -2,12 +2,30 @@ package br.com.github.sample
 
 import android.app.Application
 import android.content.Context
+import android.os.Bundle
 import android.support.test.runner.AndroidJUnitRunner
+import android.support.test.runner.lifecycle.ActivityLifecycleMonitorRegistry
+import android.support.test.runner.lifecycle.Stage
+import android.view.WindowManager
 import br.com.github.sample.application.TestApplication
 
 class MockTestRunner: AndroidJUnitRunner() {
 
     override fun newApplication(cl: ClassLoader?, className: String?, context: Context?): Application {
         return super.newApplication(cl, TestApplication::class.java.name, context)
+    }
+
+    override fun onCreate(arguments: Bundle) {
+        super.onCreate(arguments)
+
+        ActivityLifecycleMonitorRegistry.getInstance().addLifecycleCallback { activity, stage ->
+            val flags = WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
+                    WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON or
+                    WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+
+            if (stage === Stage.PRE_ON_CREATE) {
+                activity.window.addFlags(flags)
+            }
+        }
     }
 }
