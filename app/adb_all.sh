44dd86057db285c1bfb7aa63fe8c2eb12502ad2d
wakim <wakim.jraige@gmail.com>
2016-11-19 13:53:52 -0200

    Update kotlin and sdk versions. Add leakcanary noop to android tests. Minor adjustments on build gradle.

--- app/adb_all.sh
+++ app/adb_all.sh
@@ -5,16 +5,18 @@
 # ./adb+ <command> is the equivalent of ./adb -s <serial number> <command>
 #
 # Examples
-# ./adb+ version
-# ./adb+ install apidemo.apk
-# ./adb+ uninstall com.example.android.apis
+# ./adb_all adb version
+# ./adb_all adb install apidemo.apk
+# ./adb_all adb uninstall com.example.android.apis
 
-adb devices | while read line
+adb_command=$1
+
+$adb_command devices | while read line
 do
     if [ ! "$line" = "" ] && [ `echo $line | awk '{print $2}'` = "device" ]
     then
         device=`echo $line | awk '{print $1}'`
-        echo "$device $@ ..."
-        adb -s $device $@
+        echo "$device ${@:2} ..."
+        $adb_command -s $device ${@:2}
     fi
 done
