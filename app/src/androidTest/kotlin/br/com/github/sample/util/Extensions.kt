562367de68517b2a878577de265fd7044507d9b2
wakim <wakim.jraige@gmail.com>
2016-11-19 13:55:04 -0200

    Wait until idle when rotating screen.

--- app/src/androidTest/kotlin/br/com/github/sample/util/Extensions.kt
+++ app/src/androidTest/kotlin/br/com/github/sample/util/Extensions.kt
@@ -3,16 +3,29 @@ package br.com.github.sample.util
 import android.app.Activity
 import android.content.pm.ActivityInfo
 import android.support.test.InstrumentationRegistry
+import android.support.test.InstrumentationRegistry.getInstrumentation
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers.allOf
 import org.hamcrest.Matchers.equalTo
+import java.util.concurrent.CountDownLatch
+
 
 fun Activity.rotateScreen() {
+    val countDownLatch = CountDownLatch(1)
     val orientation = InstrumentationRegistry.getTargetContext().resources.configuration.orientation
     requestedOrientation = if (orientation == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) ActivityInfo.SCREEN_ORIENTATION_PORTRAIT else ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+
+    getInstrumentation().waitForIdle { countDownLatch.countDown() }
+
+    try {
+        countDownLatch.await()
+    } catch (e: InterruptedException) {
+        throw RuntimeException("Screen rotation failed", e)
+    }
+
 }
 
 fun withTag(tag: String): Matcher<View> = withTagValue(equalTo(tag))
